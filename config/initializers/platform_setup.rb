a97357a0261e013050a39982ab2dbdaef861a1b0
Arthur Del Esposte <arthurmde@gmail.com>
2016-04-27 15:43:37 -0300

    Add sample config files for resource metada and services urls

--- config/initializers/platform_setup.rb
+++ config/initializers/platform_setup.rb
@@ -1,20 +1,5 @@
-Rails.application.load_seed if Component.count == 0
+require 'yaml'
 
-json = {
-  url: "/ip/",
-  tipo: "Arduíno",
-  modelo: "Uno",
-  fabricante: "XPTO",
-  n_componentes: 0,
-  componentes: []
-}
-
-Component.all.each do |component|
-  json[:n_componentes] = json[:n_componentes] + 1 
-  # Registra no Catálogo como recurso
-  json[:componentes] << {id: component.id}
-end
-
-puts "HTTP POST url_do_catalogo/resource/registry/"
-puts json
+SERVICES_CONFIG = YAML.load_file(Rails.root.join("config", "services.yml"))
+RESOURCE_CONFIG = YAML.load_file(Rails.root.join("config", "resource.yml"))
 
