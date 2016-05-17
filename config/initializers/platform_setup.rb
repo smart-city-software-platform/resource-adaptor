ef90c4f49b65605276d692c6ab4e8b004666d69c
Arthur Del Esposte <arthurmde@gmail.com>
2016-05-16 21:37:33 -0300

    Remove dependence from original config files

--- config/initializers/platform_setup.rb
+++ config/initializers/platform_setup.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 
-SERVICES_CONFIG = YAML.load_file(Rails.root.join("config", "services.yml"))
-RESOURCE_CONFIG = YAML.load_file(Rails.root.join("config", "resource.yml"))
-
+if Rails.env.development? || Rails.env.production?
+  SERVICES_CONFIG = YAML.load_file(Rails.root.join("config", "services.yml"))
+  RESOURCE_CONFIG = YAML.load_file(Rails.root.join("config", "resource.yml"))
+end
