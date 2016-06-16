d2654f5708a123a3e2d295ebf287082ca35bf5d5
Arthur Del Esposte <arthurmde@gmail.com>
2016-06-16 15:15:41 -0300

    Improve healthy facility script

--- lib/component_services/health_facility.rb
+++ lib/component_services/health_facility.rb
@@ -3,11 +3,11 @@ module ComponentServices
     def collect_info_total_capacity
       self.last_collection['info_total_capacity']
     end
-    
+
     def collect_info_facility_type
       self.last_collection['info_facility_type']
     end
-    
+
     def collect_current_users
       Random.rand(0...(2 * collect_info_total_capacity))
     end
