e0894be6dee3a172c33e0ca1f064dc7804980462
Arthur Del Esposte <arthurmde@gmail.com>
2016-06-16 15:15:41 -0300

    Improve healthy facility script

--- lib/seeds/geosampa_importer.rb
+++ lib/seeds/geosampa_importer.rb
@@ -1,12 +1,22 @@
 require 'csv'
-require '../config/environment'
+#require '../config/environment'
+
+cont = 0
+
+puts "Importing health facilities data from GeoSampa file:"
 
 csv_text = File.open(Rails.root.join("files", "geo_sampa.csv"),"r:ISO-8859-1")
-csv = CSV.parse(csv_text, :headers => true)
-csv.each do |row|
-  puts row
+
+CSV.parse(csv_text, :headers => true).each do |row|
   component = Component.new(lat: row["lat"], lon: row["longi"], collect_interval: 60, service_type: "HealthFacility", capabilities: ['info_facility_type', 'info_total_capacity', 'current_users'])
   component.last_collection['info_facility_type'] = row["typeOf"]
   component.last_collection['info_total_capacity'] = Random.rand(50...500)
-  component.save!
+  if component.save
+    print '.'
+    cont = cont + 1
+  else
+    print 'F'
+  end
 end
+
+puts "\n#{cont} components created!"
