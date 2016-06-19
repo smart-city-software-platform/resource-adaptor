211dbf670d34b15c9c14beb41486f61e9cdfb246
Alander Marques Machado <alandermm@hotmail.com>
2016-06-19 01:22:17 +0000

    Resorce adaptors for sensors - Outdoor Sports Maps @alexmorimitsu @alandermm

--- lib/green_area_importer.rb
+++ lib/green_area_importer.rb
@@ -5,7 +5,7 @@ require 'csv'
   csv = CSV.parse(csv_text, :headers => true)
   csv.each do |row|
     puts row
-    component = Component.new(lat: row["lat"], lon: row["longi"], collect_interval: 60, service_type: "SportsMaps", capabilities: ['info_green_percentage'])
+    component = Component.new(lat: row["LAT"], lon: row["LONG"], collect_interval: 60, service_type: "SportsMaps", capabilities: ['info_green_percentage'])
     component.last_collection['info_green_percentage'] = Random.rand(0..100)
     component.save!
   end
