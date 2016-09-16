1a3b377190070e1baec0e3873f68b955e4d5c38b
Arthur Del Esposte <arthurmde@gmail.com>
2016-09-16 18:09:07 -0300

    Fix database config files
    Signed-off-by: Arthur Del Esposte <arthurmde@gmail.com>
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160427174708) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "basic_resources", force: :cascade do |t|
     t.integer  "uuid"
     t.string   "name"
