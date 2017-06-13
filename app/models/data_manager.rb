ed7d19b7240b12f47033a449bb90d3a3d37ae382
Arthur Del Esposte <arthurmde@gmail.com>
2017-06-13 14:55:43 -0300

    Returning the actuation status to the platform

--- app/models/data_manager.rb
+++ app/models/data_manager.rb
@@ -13,13 +13,20 @@ class DataManager
   def publish_resource_data(uuid, capability, value)
     message = JSON(value)
     key = uuid + '.' + capability
-    @topic.publish(message, routing_key: key)
+    topic = @channel.topic('data_stream')
+    topic.publish(message, routing_key: key)
+  end
+
+  def publish_actuation_command_status(uuid, capability, command_id, status)
+    message = JSON({command_id: command_id, status: status})
+    key = uuid + '.' + capability
+    topic = @channel.topic('resource.actuate.status')
+    topic.publish(message, routing_key: key)
   end
 
   def setup
     @conn = Bunny.new(hostname: SERVICES_CONFIG['services']['rabbitmq'])
     @conn.start
     @channel = @conn.create_channel
-    @topic = @channel.topic('data_stream')
   end
 end
--- app/workers/web_hook_caller.rb
+++ app/workers/web_hook_caller.rb
@@ -12,8 +12,10 @@ class WebHookCaller
       'capability',
       'created_at',
       'value',
+      '_id'
     )
     command['url'] = url
+    command_id = command['_id']['$oid']
 
     begin
       response = RestClient.post(
@@ -23,8 +25,10 @@ class WebHookCaller
       )
 
       WORKERS_LOGGER.info("WebHookCaller::CommandSend - notification_id: #{id}, url: #{url}")
+      DataManager.instance.publish_actuation_command_status(command['uuid'], command['capability'], command_id, "processed")
     rescue RestClient::ExceptionWithResponse => e
       WORKERS_LOGGER.error("WebHookCaller::CommandNotSend - notification_id: #{id}, url: #{url}, error: #{e.message}")
+      DataManager.instance.publish_actuation_command_status(command['uuid'], command['capability'], command_id, "rejected")
     rescue StandardError => e
       WORKERS_LOGGER.error("WebHookCaller::CommandNotSend - notification_id: #{id}, url: #{url}, error: #{e.message}")
       raise e # This will make sidekiq to retry again later
